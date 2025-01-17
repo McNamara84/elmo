b890ec30786d3381ca72d92affd9f77fbb941b8f
McNamara <holgerehrmann@gmail.com>
2025-01-17 12:30:35 +0100

    Header für JSON-Übergabe erweitert

--- getTimezoneApiKey.php
+++ getTimezoneApiKey.php
@@ -2,6 +2,7 @@
 require_once('settings.php');
 
 header('Content-Type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
 
 // Return the API key as JSON
 echo json_encode(['apiKey' => $apiKeyTimezone]);
