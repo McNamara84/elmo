e5f8f2ffad057c5600c2da7ef061656bc89ea170
McNamara <holgerehrmann@gmail.com>
2025-05-15 05:44:55 +0200

    Optimized save_data.php by conditionally saving optional fields based on visibility flags in settings.php

--- save/save_data.php
+++ save/save_data.php
@@ -39,20 +39,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    // Full save operation
+    // Saving all mandatory fields & optional fields if needed
     $resource_id = saveResourceInformationAndRights($connection, $_POST);
     saveAuthors($connection, $_POST, $resource_id);
     saveContactPerson($connection, $_POST, $resource_id);
-    saveOriginatingLaboratories($connection, $_POST, $resource_id);
-    saveContributorPersons($connection, $_POST, $resource_id);
-    saveContributorInstitutions($connection, $_POST, $resource_id);
+    if ($showMslLabs) {
+        saveOriginatingLaboratories($connection, $_POST, $resource_id);
+    }
+    if ($showContributorPersons) {
+        saveContributorPersons($connection, $_POST, $resource_id);
+    }
+    if ($showContributorInstitutions) {
+        saveContributorInstitutions($connection, $_POST, $resource_id);
+    }
     saveDescriptions($connection, $_POST, $resource_id);
-    saveKeywords($connection, $_POST, $resource_id);
-    saveFreeKeywords($connection, $_POST, $resource_id);
-    saveSpatialTemporalCoverage($connection, $_POST, $resource_id);
-    saveRelatedWork($connection, $_POST, $resource_id);
-    saveFundingReferences($connection, $_POST, $resource_id);
-    saveGGMsProperties($connection, $_POST, $resource_id);
+    if ($showGcmdThesauri) {
+        saveKeywords($connection, $_POST, $resource_id);
+    }
+    if ($showFreeKeywords) {
+        saveFreeKeywords($connection, $_POST, $resource_id);
+    }
+    if ($showSpatialTemporalCoverage) {
+        saveSpatialTemporalCoverage($connection, $_POST, $resource_id);
+    }
+    if ($showRelatedWork) {
+        saveRelatedWork($connection, $_POST, $resource_id);
+    }
+    if ($showFundingReference) {
+        saveFundingReferences($connection, $_POST, $resource_id);
+    }
+    if ($showGGMsProperties) {
+        saveGGMsProperties($connection, $_POST, $resource_id);
+    }
 
     // Handle file download if requested
     if (isset($_POST['filename'])) {
