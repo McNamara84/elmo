2502b345a96fb6e0fdeb7510f6796f10f63ed20d
Torhof <torkhov@gfz-potsdam.de>
2025-05-28 10:24:27 +0200

    populate drop down menu with data from the API endpoint: created a separate script for it

---
+++ js/eventhandlers/formgroups/ggmproperties-essential.js
@@ -0,0 +1,218 @@
+function setupICGEMFileFormats() {
+    const selectId = "#input-file-format";
+    var selectElement = $(selectId).closest(".row").find('select[name="rFileFormat[]"]');
+    const endpoint = "/vocabs/icgemformats";
+    
+    // Use a single AJAX call
+    $.ajax({
+        url: `api/v2${endpoint}`,
+        method: "GET",
+        dataType: "json",
+        
+        beforeSend: function () {
+            selectElement.prop('disabled', true);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Loading...",
+                })
+            );
+        },
+        
+        success: function (response) {
+            selectElement.empty();
+            
+            // Placeholder option
+            selectElement.append(
+                $("<option>", {
+                    value: "",
+                    text: "Choose...",
+                    "data-translate": "general.choose"
+                })
+            );
+            
+            // The response is directly an array of format objects
+            if (response && response.length > 0) {
+                // Sort formats by name if needed
+                response//.sort((a, b) => a.name.localeCompare(b.name))
+                    .forEach(function (format) {
+                        selectElement.append(
+                            $("<option>", {
+                                value: format.id,
+                                text: format.name,
+                                title: format.description
+                            })
+                        );
+                    });
+            } else {
+                selectElement.append(
+                    $("<option>", {
+                        value: "",
+                        text: "No ICGEM file formats available"
+                    })
+                );
+            }
+        },
+        
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error loading file formats:", textStatus, errorThrown);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Error loading ICGEM file formats"
+                })
+            );
+        },
+        
+        complete: function () {
+            selectElement.prop('disabled', false);
+        }
+    });
+}
+
+function setupModelTypes() {
+    const selectId = "#input-model-type" ;
+    var selectElement = $(selectId).closest(".row").find('select[name="rModelType[]"]');
+    const endpoint = "/vocabs/modeltypes";
+    
+    $.ajax({
+        url: `api/v2${endpoint}`,
+        method: "GET",
+        dataType: "json",
+        
+        beforeSend: function () {
+            selectElement.prop('disabled', true);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Loading...",
+                })
+            );
+        },
+        
+        success: function (response) {
+            selectElement.empty();
+            
+            // Placeholder option
+            selectElement.append(
+                $("<option>", {
+                    value: "",
+                    text: "Choose...",
+                    "data-translate": "general.choose"
+                })
+            );
+            
+            // The response is directly an array of format objects
+            if (response && response.length > 0) {
+                response.forEach(function (format) {
+                        selectElement.append(
+                            $("<option>", {
+                                value: format.id,
+                                text: format.name,
+                                title: format.description
+                            })
+                        );
+                    });
+            } else {
+                selectElement.append(
+                    $("<option>", {
+                        value: "",
+                        text: "No ICGEM model types available"
+                    })
+                );
+            }
+        },
+        
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error loading file formats:", textStatus, errorThrown);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Error loading ICGEM model types"
+                })
+            );
+        },
+        
+        complete: function () {
+            selectElement.prop('disabled', false);
+        }
+    });
+}
+
+function setupMathReps() {
+    const selectId = "#input-mathematical-representation" ;
+    var selectElement = $(selectId).closest(".row").find('select[name="rMathematicalRepresentation[]"]');
+    const endpoint = "/vocabs/mathreps";
+    
+    $.ajax({
+        url: `api/v2${endpoint}`,
+        method: "GET",
+        dataType: "json",
+        
+        beforeSend: function () {
+            selectElement.prop('disabled', true);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Loading...",
+                })
+            );
+        },
+        
+        success: function (response) {
+            selectElement.empty();
+            
+            // Placeholder option
+            selectElement.append(
+                $("<option>", {
+                    value: "",
+                    text: "Choose...",
+                    "data-translate": "general.choose"
+                })
+            );
+            
+            // The response is directly an array of format objects
+            if (response && response.length > 0) {
+                // Sort formats by name if needed
+                response//.sort((a, b) => a.name.localeCompare(b.name))
+                    .forEach(function (format) {
+                        selectElement.append(
+                            $("<option>", {
+                                value: format.id,
+                                text: format.name,
+                                title: format.description
+                            })
+                        );
+                    });
+            } else {
+                selectElement.append(
+                    $("<option>", {
+                        value: "",
+                        text: "No ICGEM mathematical representations available"
+                    })
+                );
+            }
+        },
+        
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error loading file formats:", textStatus, errorThrown);
+            selectElement.empty().append(
+                $("<option>", {
+                    value: "",
+                    text: "Error loading ICGEM mathematical representations"
+                })
+            );
+        },
+        
+        complete: function () {
+            selectElement.prop('disabled', false);
+        }
+    });
+}
+
+// Call the function when document is ready
+$(document).ready(function() {
+    setupICGEMFileFormats();
+    setupModelTypes();
+    setupMathReps();
+});
