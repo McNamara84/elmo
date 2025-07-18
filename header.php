57557378224f327fb51dbfe28ede436eb90a8819
McNamara <holgerehrmann@gmail.com>
2025-07-18 15:43:44 +0200

    Switch Tagify CSS from CDN to local file
    Updated the Tagify CSS link in header.php to use the local file from node_modules instead of the CDN. This may improve load times and reliability for local development.

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ sort($langCodes);
     href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css"
     integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- CDN for Tagify CSS -->
+  <!-- Local Tagify CSS -->
   <link href="node_modules\@yaireo\tagify\dist\tagify.css" rel="stylesheet" type="text/css" />
   <!-- CDN for jsTree CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.17/themes/default/style.min.css"
