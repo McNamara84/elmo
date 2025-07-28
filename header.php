cb0db1cab05c6fa3b6f32354a3230548c2bed29c
McNamara <holgerehrmann@gmail.com>
2025-07-28 12:19:53 +0200

    Switch Bootstrap CSS from CDN to local file
    Replaced the Bootstrap CSS CDN link with a local file reference to use the version installed in node_modules. This improves reliability and allows for offline development.

--- header.php
+++ header.php
@@ -10,9 +10,8 @@ sort($langCodes);
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <!-- CDN for Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Local Bootstrap CSS -->
+  <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Local jQuery UI CSS -->
   <link rel="stylesheet" href="node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css" />
   <!-- CDN for Bootstrap Icons CSS -->
