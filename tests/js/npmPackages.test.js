d1a198c8339aa716385c25b826688f4e5b65c53a
McNamara <holgerehrmann@gmail.com>
2025-07-28 13:26:23 +0200

    Add test to verify npm package installations
    Introduces a test that checks if all dependencies and devDependencies listed in package.json have corresponding directories in node_modules, ensuring packages are properly installed.

---
+++ tests/js/npmPackages.test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const packageJson = require('../../package.json');
+
+const dependencies = {
+  ...(packageJson.dependencies || {}),
+  ...(packageJson.devDependencies || {}),
+};
+
+const nodeModulesDir = path.join(__dirname, '..', '..', 'node_modules');
+
+describe('npm packages installation', () => {
+  Object.keys(dependencies).forEach((pkg) => {
+    test(`package '${pkg}' directory exists`, () => {
+      const pkgDir = path.join(nodeModulesDir, pkg);
+      expect(fs.existsSync(pkgDir)).toBe(true);
+      const stat = fs.lstatSync(pkgDir);
+      if (stat.isSymbolicLink()) {
+        const realPath = fs.realpathSync(pkgDir);
+        expect(fs.existsSync(realPath)).toBe(true);
+      }
+    });
+  });
+});
