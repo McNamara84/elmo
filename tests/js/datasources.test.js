95e7f270775df9c733c7ada1aba5ad36e4d5374d
McNamara <holgerehrmann@gmail.com>
2025-08-01 11:18:12 +0200

    Add tests for datasources.js UI behavior
    Introduces a new test file for datasources.js, including a mock Tagify class and DOM setup to verify initial row visibility for datasource type 'S'. This helps ensure correct UI behavior and improves test coverage.

---
+++ tests/js/datasources.test.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const path = require('path');
+
+class MockTagify {
+  constructor(el, settings) {
+    this.el = el;
+    this.settings = settings;
+    this._callbacks = {};
+  }
+  on(event, cb) {
+    this._callbacks[event] = cb;
+  }
+  addTags() {}
+  removeAllTags() {}
+}
+
+describe('datasources.js', () => {
+  let $;
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="group-datasources">
+        <div class="row">
+          <div class="visibility-datasources-basic">
+            <select name="datasource_type[]">
+              <option value="S" selected>Satellite</option>
+              <option value="G">Ground</option>
+              <option value="A">Altimetry</option>
+              <option value="T">Terrain</option>
+              <option value="M">Model</option>
+            </select>
+          </div>
+          <div class="visibility-datasources-details"><select name="datasource_details[]"></select></div>
+          <div class="visibility-datasources-satellite">sat</div>
+          <div class="visibility-datasources-identifier"><select name="dIdentifierType[]"></select></div>
+          <div class="input-group">
+            <input class="input-with-help" />
+            <div class="help-placeholder" data-help-section-id="ds"></div>
+          </div>
+          <input id="input-datasource-platforms" />
+          <button class="addDataSource"></button>
+        </div>
+      </div>
+      <div id="jstree-platforms-datasource"></div>
+      <ul id="selected-keywords-platforms-ds"></ul>
+      <div id="modal-platforms-datasource"></div>
+    `;
+    localStorage.setItem('helpStatus', 'help-on');
+
+    $ = require('jquery');
+    global.$ = $;
+    global.jQuery = $;
+    window.$ = $;
+    window.jQuery = $;
+
+    const originalIs = $.fn.is;
+    $.fn.is = function(selector) {
+      if (selector === ':visible') {
+        return this.css('display') !== 'none';
+      }
+      return originalIs.call(this, selector);
+    };
+
+    global.createRemoveButton = jest.fn(() => $('<button class="removeButton"></button>'));
+    global.replaceHelpButtonInClonedRows = jest.fn();
+    global.setupIdentifierTypesDropdown = jest.fn(select => {
+      select.append('<option value="id">id</option>');
+    });
+    global.Tagify = MockTagify;
+
+    let script = fs.readFileSync(path.resolve(__dirname, '../../js/eventhandlers/formgroups/datasources.js'), 'utf8');
+    script = script.replace("import { createRemoveButton, replaceHelpButtonInClonedRows } from '../functions.js';", 'const { createRemoveButton, replaceHelpButtonInClonedRows } = window;');
+    script = script.replace('$(document).ready(function () {', '(function () {');
+    script = script.replace(/\n\}\);$/, '\n})();');
+    window.eval(script);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    delete global.createRemoveButton;
+    delete global.replaceHelpButtonInClonedRows;
+    delete global.setupIdentifierTypesDropdown;
+    delete global.Tagify;
+  });
+
+  test('initial row visibility is correct for type S', () => {
+    const row = $('#group-datasources .row').first();
+    expect(row.children('.visibility-datasources-details').css('display')).toBe('none');
+    expect(row.children('.visibility-datasources-satellite').css('display')).not.toBe('none');
+    expect(row.children('.visibility-datasources-identifier').css('display')).toBe('none');
+  });
+});
