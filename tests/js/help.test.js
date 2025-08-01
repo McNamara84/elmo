fa48e80b1daab39ae4ae1d184af3b24f98be1ed5
McNamara <holgerehrmann@gmail.com>
2025-08-01 12:59:00 +0200

    Add unit test for help.js initialization
    Introduces a Jest test to verify that help.js initializes UI elements to the correct default state, including button and icon classes. This ensures the help feature is enabled by default and UI elements reflect the expected active state.

---
+++ tests/js/help.test.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+describe('help.js', () => {
+  let $;
+
+  const loadScript = () => {
+    const script = fs.readFileSync(path.resolve(__dirname, '../../js/help.js'), 'utf8');
+    const match = script.match(/\$\(document\)\.ready\(function \(\) {([\s\S]*)}\);/);
+    if (match) {
+      const fn = new Function(match[1]);
+      fn();
+    } else {
+      throw new Error('Unable to parse help.js');
+    }
+  };
+
+  beforeEach(() => {
+    jest.resetModules();
+    document.body.innerHTML = `
+      <button id="buttonHelpOn"></button>
+      <i id="bd-help-icon"></i>
+      <button id="buttonHelpOff"></button>
+      <input class="input-with-help" />
+      <div id="helpModal"><div class="modal-body"></div></div>
+      <button id="buttonHelp"></button>
+      <div id="helpIcon" data-help-section-id="section1"></div>
+    `;
+    localStorage.clear();
+    $ = require('jquery');
+    global.$ = $;
+    global.jQuery = $;
+    $.fn.modal = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    delete global.$;
+    delete global.jQuery;
+  });
+
+  test('initializes to help-on by default', () => {
+    loadScript();
+    expect($('#buttonHelpOn').hasClass('active')).toBe(true);
+    expect($('#buttonHelpOff').hasClass('active')).toBe(false);
+    expect($('#bd-help-icon').hasClass('bi-question-square-fill')).toBe(true);
+    expect($('.input-with-help').hasClass('input-right-no-round-corners')).toBe(true);
+  });
+});
