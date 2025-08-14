2261431ce051e9997da55e3097e56325ea6e15da
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-08-14 10:41:44 +0200

    Aufrufe von replaceHelpButtonInClonedRows in "contributor-person.js" umpositioniert, damit sie auf alle Felder wirken kann

--- js/eventhandlers/formgroups/contributor-person.js
+++ js/eventhandlers/formgroups/contributor-person.js
@@ -31,8 +31,6 @@ $(document).ready(function () {
     newContributorRow.find(".row-label").hide();
     newContributorRow.find("input").removeAttr("required");
 
-    // Replace help buttons with invisible placeholders
-    replaceHelpButtonInClonedRows(newContributorRow);
 
     // Replace role field
     const roleFieldHtml = `
@@ -58,6 +56,9 @@ $(document).ready(function () {
       </div>
     `;
     newContributorRow.find("#input-contributorpersons-affiliation").closest(".input-group").replaceWith(affFieldHtml);
+
+    // Replace help buttons with invisible placeholders
+    replaceHelpButtonInClonedRows(newContributorRow);
 
     // Update remaining input IDs
     newContributorRow.find("#input-contributor-orcid").attr("id", "input-contributor-orcid" + uniqueSuffix);
