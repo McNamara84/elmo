8331c03e05905000b0bba656e89323c31920985c
Torhof <torkhov@gfz-potsdam.de>
2025-08-19 15:01:56 +0200

    isolated the processing of keywords with limited trees for ICGEM data sources into the data sources file

--- js/thesauri.js
+++ js/thesauri.js
@@ -41,15 +41,6 @@ $(document).ready(function () {
             jsTreeId: '#jstree-mslkeyword',
             searchInputId: '#input-mslkeyword-thesaurussearch',
             selectedKeywordsListId: 'selected-keywords-msl'
-        },
-        {
-            // Configuration for the Data Sources GCMD Platform input
-            inputId: '#input-datasource-platforms',
-            jsonFile: 'json/thesauri/gcmdPlatformsKeywords.json',
-            jsTreeId: '#jstree-platforms-datasource',
-            searchInputId: '#input-platforms-thesaurussearch-ds',
-            selectedKeywordsListId: 'selected-keywords-platforms-ds',
-            rootNodeId: 'https://gcmd.earthdata.nasa.gov/kms/concept/b39a69b4-c3b9-4a94-b296-bbbbe5e4c847'
         }
     ];
 
@@ -228,102 +219,93 @@ $(document).ready(function () {
                 $(config.jsTreeId).jstree(true).search($(this).val());
             });
 
-            if (config.inputId !== '#input-datasource-platforms') {
-                function updateSelectedKeywordsList() {
-                    let selectedKeywordsList = document.getElementById(config.selectedKeywordsListId);
-                    if (!selectedKeywordsList) return;
-<<<<<<< HEAD
-=======
-            
             function updateSelectedKeywordsList() {
                 let selectedKeywordsList = document.getElementById(config.selectedKeywordsListId);
                 if (!selectedKeywordsList) return;
->>>>>>> 84fafdb3f644591c1f385439040615b266bedf27
 
-                    selectedKeywordsList.innerHTML = "";
-                    var selectedNodes = $(config.jsTreeId).jstree("get_selected", true);
+                selectedKeywordsList.innerHTML = "";
+                var selectedNodes = $(config.jsTreeId).jstree("get_selected", true);
 
-                    selectedNodes.forEach(function (node) {
-                        let fullPath = $(config.jsTreeId).jstree().get_path(node, " > ");
-                        let listItem = document.createElement("li");
-                        listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-                        listItem.textContent = fullPath;
+                selectedNodes.forEach(function (node) {
+                    let fullPath = $(config.jsTreeId).jstree().get_path(node, " > ");
+                    let listItem = document.createElement("li");
+                    listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+                    listItem.textContent = fullPath;
 
-                        let removeButton = document.createElement("button");
-                        removeButton.classList.add("btn", "btn-sm", "btn-danger");
-                        removeButton.innerHTML = "&times;";
-                        removeButton.onclick = function () {
-                            $(config.jsTreeId).jstree("deselect_node", node.id);
-                        };
+                    let removeButton = document.createElement("button");
+                    removeButton.classList.add("btn", "btn-sm", "btn-danger");
+                    removeButton.innerHTML = "&times;";
+                    removeButton.onclick = function () {
+                        $(config.jsTreeId).jstree("deselect_node", node.id);
+                    };
 
-                        listItem.appendChild(removeButton);
-                        selectedKeywordsList.appendChild(listItem);
-                    });
+                    listItem.appendChild(removeButton);
+                    selectedKeywordsList.appendChild(listItem);
+                });
+            }
+
+            // Event handler for 'changed.jstree'
+            $(config.jsTreeId).on("changed.jstree", function (e, data) {
+                updateSelectedKeywordsList();
+
+                // Updates the Tagify tags based on the jsTree selection
+                var selectedNodes = $(config.jsTreeId).jstree("get_selected", true);
+                var selectedValues = selectedNodes.map(function (node) {
+                    return data.instance.get_path(node, " > ");
+                });
+
+                thesaurusKeywordstagify.removeAllTags();
+                thesaurusKeywordstagify.addTags(selectedValues);
+            });
+
+            /**
+            * Event handler for when a tag is added to Tagify.
+            * The function selects the corresponding node in jsTree based on the tag text.
+            *
+            * @param {Event} e - The event triggered by adding a tag to Tagify.
+            * @param {Object} e.detail - The details of the event.
+            * @param {Object} e.detail.data - The data of the added tag.
+            * @param {string} e.detail.data.value - The value of the added tag.
+            */
+            thesaurusKeywordstagify.on('add', function (e) {
+                var tagText = e.detail.data.value;
+                var jsTree = $(config.jsTreeId).jstree(true);
+                var node = findNodeByPath(jsTree, tagText);
+                if (node) {
+                    jsTree.select_node(node.id);
                 }
+            });
 
-                // Event handler for 'changed.jstree'
-                $(config.jsTreeId).on("changed.jstree", function (e, data) {
-                    updateSelectedKeywordsList();
-
-                    // Updates the Tagify tags based on the jsTree selection
-                    var selectedNodes = $(config.jsTreeId).jstree("get_selected", true);
-                    var selectedValues = selectedNodes.map(function (node) {
-                        return data.instance.get_path(node, " > ");
-                    });
-
-                    thesaurusKeywordstagify.removeAllTags();
-                    thesaurusKeywordstagify.addTags(selectedValues);
-                });
-
-                /**
-                * Event handler for when a tag is added to Tagify.
-                * The function selects the corresponding node in jsTree based on the tag text.
-                *
-                * @param {Event} e - The event triggered by adding a tag to Tagify.
-                * @param {Object} e.detail - The details of the event.
-                * @param {Object} e.detail.data - The data of the added tag.
-                * @param {string} e.detail.data.value - The value of the added tag.
-                */
-                thesaurusKeywordstagify.on('add', function (e) {
-                    var tagText = e.detail.data.value;
-                    var jsTree = $(config.jsTreeId).jstree(true);
-                    var node = findNodeByPath(jsTree, tagText);
-                    if (node) {
-                        jsTree.select_node(node.id);
-                    }
-                });
-
-                /**
-                * Event handler for when a tag is removed from Tagify.
-                * The function deselects the corresponding node in jsTree based on the removed tag.
-                *
-                * @param {Event} e - The event triggered by removing a tag from Tagify.
-                * @param {Object} e.detail - The details of the event.
-                * @param {Object} e.detail.data - The data of the removed tag.
-                * @param {string} e.detail.data.value - The value of the removed tag.
-                */
-                thesaurusKeywordstagify.on('remove', function (e) {
-                    var tagText = e.detail.data.value;
-                    var jsTree = $(config.jsTreeId).jstree(true);
-                    var node = findNodeByPath(jsTree, tagText);
-                    if (node) {
-                        jsTree.deselect_node(node.id);
-                    }
-                });
-
-                /**
-                * Finds a node in the jsTree by its full path.
-                * This function searches through all the nodes in the jsTree and returns the node that matches the provided path.
-                *
-                * @param {Object} jsTree - The jsTree instance to search through.
-                * @param {string} path - The full path of the node to find, formatted as a string with " > " separators.
-                * @returns {Object|null} The node object if found, or `null` if no node matches the path.
-                */
-                function findNodeByPath(jsTree, path) {
-                    return jsTree.get_json("#", { flat: true }).find(function (n) {
-                        return jsTree.get_path(n, " > ") === path;
-                    });
+            /**
+            * Event handler for when a tag is removed from Tagify.
+            * The function deselects the corresponding node in jsTree based on the removed tag.
+            *
+            * @param {Event} e - The event triggered by removing a tag from Tagify.
+            * @param {Object} e.detail - The details of the event.
+            * @param {Object} e.detail.data - The data of the removed tag.
+            * @param {string} e.detail.data.value - The value of the removed tag.
+            */
+            thesaurusKeywordstagify.on('remove', function (e) {
+                var tagText = e.detail.data.value;
+                var jsTree = $(config.jsTreeId).jstree(true);
+                var node = findNodeByPath(jsTree, tagText);
+                if (node) {
+                    jsTree.deselect_node(node.id);
                 }
+            });
+
+            /**
+            * Finds a node in the jsTree by its full path.
+            * This function searches through all the nodes in the jsTree and returns the node that matches the provided path.
+            *
+            * @param {Object} jsTree - The jsTree instance to search through.
+            * @param {string} path - The full path of the node to find, formatted as a string with " > " separators.
+            * @returns {Object|null} The node object if found, or `null` if no node matches the path.
+            */
+            function findNodeByPath(jsTree, path) {
+                return jsTree.get_json("#", { flat: true }).find(function (n) {
+                    return jsTree.get_path(n, " > ") === path;
+                });
             }
         }
 
