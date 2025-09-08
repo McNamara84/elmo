77d9afa380e16ac8dd22bbd15b721a5435305253
Torhof <torkhov@gfz-potsdam.de>
2025-09-08 11:33:42 +0200

    set timeout to 0.1 seconds for both orcid tests

--- tests/playwright/orcid-autocomplete.spec.ts
+++ tests/playwright/orcid-autocomplete.spec.ts
@@ -5,7 +5,7 @@ test.describe('ORCID API autocomplete tests', () => {
     await page.goto('');
     await page.locator('#input-author-orcid').fill('0000-0002-1825-0097');
     await page.getByRole('textbox', { name: 'First Name*' }).click();
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(100); // wait for 0.1 seconds 
     await expect(page.getByRole('textbox', { name: 'First Name*' })).toHaveValue('Josiah');
     await expect(page.getByRole('textbox', { name: 'Last Name*' })).toHaveValue('Carberry');
   });
@@ -13,6 +13,7 @@ test.describe('ORCID API autocomplete tests', () => {
   await page.goto('');
   await page.locator('#input-author-orcid').fill('0000-0001-5140-8602');
   await page.getByRole('textbox', { name: 'First Name*' }).click();
+  await page.waitForTimeout(100); // wait for 0.1 seconds 
   await expect(page.locator('tag')).toContainText('Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences');
   });
 });
