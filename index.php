b8a3c8ae4a56775b79635803111cbcc8c2b72688
Torhof <torkhov@gfz-potsdam.de>
2025-09-22 11:32:51 +0200

    added a sample file for choice and included it conditionally into index

--- index.php
+++ index.php
@@ -20,7 +20,11 @@ include_once $baseDir ."helper_functions.php";
 
 loadEnvVariables();
 // Second, include variables subjected to change
-include_once $baseDir ."choice.php";
+if (file_exists($baseDir . "choice.php")) {
+    include_once $baseDir . "choice.php";
+} else {
+    include_once $baseDir . "choice.sample.php";
+}
 
 // Include HTML components using absolute paths to ensure reliable file access
 $baseDir = __DIR__ . '/';
