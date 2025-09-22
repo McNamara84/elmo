dd92bdbe979300d202e0a09938a56e6f9ee0501b
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-09-22 12:58:33 +0200

    Refactoring and improving contributor person form group handling

--- js/eventhandlers/formgroups/contributor-person.js
+++ js/eventhandlers/formgroups/contributor-person.js
@@ -17,84 +17,63 @@ $(document).ready(function () {
    * @requires jQuery
    * @requires Tagify
    */
+  let personIndex = 1; // Start bei 1, Originalzeile = 0
+
   $("#button-contributor-addperson").click(function () {
     const contributorGroup = $("#group-contributorperson");
     const firstContributorRow = contributorGroup.children().first();
-    const uniqueSuffix = new Date().getTime();
-
     const newContributorRow = firstContributorRow.clone();
 
-    // Reset form fields and validation state
-    newContributorRow.find("input").val("").removeClass("is-invalid is-valid");
-    newContributorRow.find(".tagify").remove();
+    // Reset input fields & validation
+    newContributorRow.find("input").val("").removeClass("is-invalid is-valid").removeAttr("required");
     newContributorRow.find(".invalid-feedback, .valid-feedback").css("display", "");
+    newContributorRow.find(".tagify").remove();
     newContributorRow.find(".row-label").hide();
-    newContributorRow.find("input").removeAttr("required");
 
-
-    // Replace role field
-    const roleFieldHtml = `
-      <div class="input-group has-validation">
-        <input name="cbPersonRoles[]" id="input-contributor-personrole${uniqueSuffix}"
-          class="form-control tagify--custom-dropdown input-with-help input-right-no-round-corners"
-          data-translate-placeholder="general.roleLabel" />
-        <span class="input-group-text"><i class="bi bi-question-circle-fill" data-help-section-id="help-contributor-role"></i></span>
-        <div class="invalid-feedback" data-translate="general.pleaseChoose">Please choose</div>
-      </div>
-    `;
-    newContributorRow.find("#input-contributor-personrole").closest(".input-group").replaceWith(roleFieldHtml);
-
-    // Replace affiliation field
-    const affFieldHtml = `
-      <div class="input-group has-validation">
-        <input type="text" name="cbPersonAffiliations[]" id="input-contributorpersons-affiliation${uniqueSuffix}"
-          class="form-control input-with-help input-right-no-round-corners" 
-          data-translate-placeholder="general.affiliation" />
-        <input type="hidden" name="cbPersonRorIds[]" id="input-contributor-personrorid${uniqueSuffix}" />
-        <span class="input-group-text"><i class="bi bi-question-circle-fill" data-help-section-id="help-contributor-affiliation"></i></span>
-        <div class="invalid-feedback" data-translate="general.pleaseChoose">Please choose</div>
-      </div>
-    `;
-    newContributorRow.find("#input-contributorpersons-affiliation").closest(".input-group").replaceWith(affFieldHtml);
-
-    // Replace help buttons with invisible placeholders
+    // Replace help buttons
     replaceHelpButtonInClonedRows(newContributorRow);
 
-    // Update remaining input IDs
-    newContributorRow.find("#input-contributor-orcid").attr("id", "input-contributor-orcid" + uniqueSuffix);
-    newContributorRow.find("#input-contributor-lastname").attr("id", "input-contributor-lastname" + uniqueSuffix);
-    newContributorRow.find("#input-contributor-firstname").attr("id", "input-contributor-firstname" + uniqueSuffix);
-
-    // Update label references
-    const labelMappings = ["orcid", "lastname", "firstname", "personrole"];
-    labelMappings.forEach(label => {
-      newContributorRow
-        .find(`label[for='input-contributor-${label}']`)
-        .attr("for", `input-contributor-${label}${uniqueSuffix}`);
-    });
-
-    // Replace add button with remove button
+    // Replace Add button with Remove button
     newContributorRow.find(".addContributorPerson").replaceWith(createRemoveButton());
 
-    // Append the new row to the DOM
+    // Generate new IDs based on row index
+    const nameId = `input-contributor-firstname-${personIndex}`;
+    const lastnameId = `input-contributor-lastname-${personIndex}`;
+    const orcidId = `input-contributor-orcid-${personIndex}`;
+    const roleId = `input-contributor-personrole-${personIndex}`;
+    const affId = `input-contributorpersons-affiliation-${personIndex}`;
+    const rorId = `input-contributor-personrorid-${personIndex}`;
+
+    // Update input IDs
+    newContributorRow.find("#input-contributor-firstname").attr("id", nameId);
+    newContributorRow.find("#input-contributor-lastname").attr("id", lastnameId);
+    newContributorRow.find("#input-contributor-orcid").attr("id", orcidId);
+    newContributorRow.find("#input-contributor-personrole").attr("id", roleId);
+    newContributorRow.find("#input-contributorpersons-affiliation").attr("id", affId);
+    newContributorRow.find("#input-contributor-personrorid").attr("id", rorId);
+
+    // Update label references
+    newContributorRow.find("label[for='input-contributor-firstname']").attr("for", nameId);
+    newContributorRow.find("label[for='input-contributor-lastname']").attr("for", lastnameId);
+    newContributorRow.find("label[for='input-contributor-orcid']").attr("for", orcidId);
+    newContributorRow.find("label[for='input-contributor-personrole']").attr("for", roleId);
+
+    // Append the new row
     contributorGroup.append(newContributorRow);
 
     // Initialize Tagify for roles
-    setupRolesDropdown(["person", "both"], `#input-contributor-personrole${uniqueSuffix}`);
-
-    // Check if affiliationsData is available in global scope
+    setupRolesDropdown(["person", "both"], `#${roleId}`);
     if (window.affiliationsData && Array.isArray(window.affiliationsData)) {
-      autocompleteAffiliations(
-        `input-contributorpersons-affiliation${uniqueSuffix}`,
-        `input-contributor-personrorid${uniqueSuffix}`,
-        window.affiliationsData
-      );
+      autocompleteAffiliations(affId, rorId, window.affiliationsData);
     }
 
-    // Enable removal of the new row
+    // Remove button handler
     newContributorRow.on("click", ".removeButton", function () {
       $(this).closest(".row").remove();
       validateAllMandatoryFields();
     });
+    
+    // Increment index for next addition
+    personIndex++;
   });
 });
