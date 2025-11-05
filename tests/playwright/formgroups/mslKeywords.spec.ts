9a8eb5add5abbfacf4cff37dba466c6b5e2e8eb7
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-06 00:27:18 +0100

    Refactoring of Playwright tests for EPOS Multi-Scale Laboratories Keywords (MSL)

--- tests/playwright/formgroups/mslKeywords.spec.ts
+++ tests/playwright/formgroups/mslKeywords.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { enableHelp, expectNavbarVisible, navigateToHome, SELECTORS, } from '../utils';
 
-test.describe('EPOS Multi-Scale Laboratories Keywords (MSL)', () => {
+  test.describe("EPOS Multi-Scale Laboratories Keywords (MSL)", () => {
   test.beforeEach(async ({ page }) => {
     await navigateToHome(page);
     await expectNavbarVisible(page);
-    await expect(page.locator(SELECTORS.formGroups.mslkeyword)).toBeVisible();
+    await expect(page.locator('#input-mslkeyword')).toBeVisible();
   });
 
   test('MSL Keyword input and thesaurus modal open correctly', async ({ page }) => {
