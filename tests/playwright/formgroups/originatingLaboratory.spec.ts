d1869ed899514dcc65389fcadb1621bdf938c6ea
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-05 14:57:52 +0100

    add playwright-test for originatinglaboratory

--- tests/playwright/formgroups/originatingLaboratory.spec.ts
+++ tests/playwright/formgroups/originatingLaboratory.spec.ts
@@ -2,10 +2,25 @@ import { test, expect } from '@playwright/test';
 import { enableHelp, expectNavbarVisible, navigateToHome, SELECTORS } from '../utils';
 
 test.describe('Originating Laboratory', () => {
-  test.beforeEach(async ({ page }) => {
-    await navigateToHome(page);
-    await expectNavbarVisible(page);
-    await expect(page.locator(SELECTORS.formGroups.originatingLaboratory)).toBeVisible();
-  });
+    test.beforeEach(async ({ page }) => {
+        await navigateToHome(page);
+        await expectNavbarVisible(page);
+        await expect(page.locator(SELECTORS.formGroups.originatingLaboratory)).toBeVisible();
+    });
+
+    test('Laboratory select loads options from JSON', async ({ page }) => {
+        // Warte hier auf das Laden der Optionen
+        await page.waitForFunction(() =>
+            document.querySelectorAll('#input-originatinglaboratory-name option').length > 1
+        );
+
+        const select = page.locator('#input-originatinglaboratory-name');
+        const options = select.locator('option');
+        const count = await options.count();
+        expect(count).toBeGreaterThan(1);
+
+        const optionTexts = await options.allTextContents();
+        expect(optionTexts.join(' ')).toContain('');
+    });
 
 });
