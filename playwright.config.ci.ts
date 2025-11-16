54244cb9dfdcc6b4781a383e1542fa658181c496
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-16 20:42:13 +0100

    Revert "timeout playwright.config.ci has been increased"
    This reverts commit b4d7a42e6d39c1d66da5d38d1c4d103d5036bcf4.

--- playwright.config.ci.ts
+++ playwright.config.ci.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   testDir: './tests/playwright',
   timeout: 100000, 
   expect: {
-    timeout: 300000,
+    timeout: 15000,
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
