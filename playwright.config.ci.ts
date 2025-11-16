8d71d6327596f721c4abdedc8b4f2062461daa95
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-16 14:12:00 +0100

    Update playwright.config.ci.ts

--- playwright.config.ci.ts
+++ playwright.config.ci.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',           
     url: 'http://localhost:8000/',      
-    reuseExistingServer: !process.env.CI, 
+    reuseExistingServer: true,
     timeout: 120 * 1000,                
   },
   projects: [
