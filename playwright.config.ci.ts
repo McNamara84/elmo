1bfbf67521df572a4d98001b7a2ee535bf0b5ced
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-16 20:42:25 +0100

    Revert "added timeouts in playwroght.config.ci"
    This reverts commit 77cac3cd2cbf9a734e06ab07b18a76328614124c.

--- playwright.config.ci.ts
+++ playwright.config.ci.ts
@@ -3,10 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/playwright',
-  timeout: 100000, 
-  expect: {
-    timeout: 15000,
-  },
+  timeout: 100000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -20,8 +17,6 @@ export default defineConfig({
     trace: 'on-first-retry',
     video: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    actionTimeout: 15000,      // Timeout for Actions
-    navigationTimeout: 30000,  // Timeout for Navigations
   },
   projects: [
     {
