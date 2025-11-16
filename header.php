08481621a77b878b36ec1c6e9ea2dc647681a6a6
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-16 16:14:51 +0100

    Add hidden H1 element for accessibility in the header

--- header.php
+++ header.php
@@ -43,6 +43,7 @@ if (!empty($_SERVER['HTTP_X_FORWARDED_PREFIX'])) {
 <body>
   <!-- Fixed Header -->
   <header class="navbar navbar-primary sticky-top bg-primary flex-md-nowrap p-0 shadow" role="banner">
+    <h1 class="visually-hidden">ELMO</h1>
     <!-- <h1 class="visually-hidden" data-translate="general.logoTitle">
       ELMO - Enhanced Laboratory Metadata Organizer 2.0
     </h1>
