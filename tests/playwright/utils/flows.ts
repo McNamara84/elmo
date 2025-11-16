2d53451a9f572822b9af1b1b4bacb408571f8726
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-16 21:00:17 +0100

    Update flows.ts

--- tests/playwright/utils/flows.ts
+++ tests/playwright/utils/flows.ts
@@ -1,26 +1,26 @@
 import { expect, type Page } from '@playwright/test';
 
 export async function completeMinimalDatasetForm(page: Page) {
-  await page.getByRole('textbox', { name: 'Publication Year (YYYY)*' }).fill('2025');
-  await page.getByLabel('Resource Type*').selectOption('5');
-  await page.getByLabel('Language of dataset*').selectOption('1');
-  await page.getByRole('textbox', { name: 'Title*' }).fill('A dataset');
+  await page.getByRole('textbox', { name: 'Publication Year (YYYY)*' }).fill('2025', { timeout: 10000 });
+  await page.getByLabel('Resource Type*').selectOption('5', { timeout: 10000 });
+  await page.getByLabel('Language of dataset*').selectOption('1', { timeout: 10000 });
+  await page.getByRole('textbox', { name: 'Title*' }).fill('A dataset', { timeout: 10000 });
 
-  await page.locator('#input-author-orcid').fill('0000-0002-1825-0097');
-  await page.getByRole('textbox', { name: 'Last Name*' }).fill('Alice');
-  await page.getByRole('textbox', { name: 'First Name*' }).fill('Bob');
+  await page.locator('#input-author-orcid').fill('0000-0002-1825-0097', { timeout: 10000 });
+  await page.getByRole('textbox', { name: 'Last Name*' }).fill('Alice', { timeout: 10000 });
+  await page.getByRole('textbox', { name: 'First Name*' }).fill('Bob', { timeout: 10000 });
 
   await page
     .locator('#group-author tags')
     .getByRole('textbox')
-    .fill('GFZ Helmholtz Centre for Geosciences');
+    .fill('GFZ Helmholtz Centre for Geosciences', { timeout: 10000 });
 
-  await page.getByText('ContactPerson?').click();
+  await page.getByText('ContactPerson?').click({ timeout: 10000 });
 
   const emailField = page.getByRole('textbox', { name: 'Email address*' });
-  await expect(emailField).toBeVisible();
-  await emailField.fill('example@gmail.com');
+  await expect(emailField).toBeVisible({ timeout: 10000 });
+  await emailField.fill('example@gmail.com', { timeout: 10000 });
 
-  await page.getByRole('textbox', { name: 'Abstract*' }).fill('Necessary abstract');
-  await page.getByRole('textbox', { name: 'Date created*' }).fill('2025-01-01');
+  await page.getByRole('textbox', { name: 'Abstract*' }).fill('Necessary abstract', { timeout: 10000  });
+  await page.getByRole('textbox', { name: 'Date created*' }).fill('2025-01-01', { timeout: 10000  });
 }
