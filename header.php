9fd5d5a6e66fa6a47749c8fc2c2e3d13b84c3c85
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-11-21 10:41:50 +0100

    title has been adjusted

--- header.php
+++ header.php
@@ -127,7 +127,7 @@ if (!empty($_SERVER['HTTP_X_FORWARDED_PREFIX'])) {
   <section role="region" aria-label="Page subheader">
     <div class="bg-white border-bottom shadow-sm">
       <div class="container-fluid py-2 d-flex justify-content-between align-items-center">
-        <div class="fs-5 fw-semibold text-dark">ELMO – GFZ Data Services' Metadata Editor 2.0</div>
+        <div class="fs-5 fw-semibold text-dark">ELMO – GFZ Metadata Editor 2.0</div>
         <?php echo $msl_logo; ?>
       </div>
     </div>
