49d61968b464a16f6080b078cfcc5028dcdb4404
Torhof <torkhov@gfz-potsdam.de>
2025-12-04 11:51:26 +0100

    big renaming and add all the required files. Reason: what waas called model properties is now called model definition. What was called technical is now model properties.

---
+++ save/formgroups/save_ggms_modeltypes.php
@@ -0,0 +1,12 @@
+<?php 
+
+a place to record the variables specific to each and every model type: temporal, static, topographic.
+Possibly othher types in the future.
+ * Saves the Gravity Gradient Model (GGM) model types to the database.
+ *
+ * @param mysqli $connection  The database connection.
+ * @param array  $postData    The POST data from the form.
+ * @param int    $resource_id The ID of the associated resource.
+ *
+ * @return void
+ */
---
+++ save/formgroups/save_ggms_properties.php
@@ -0,0 +1,11 @@
+<?php 
+
+a place to save into the database the maximum degree of a model, and other properties
+ * Saves the Gravity Gradient Model (GGM) properties to the database.
+ *
+ * @param mysqli $connection  The database connection.
+ * @param array  $postData    The POST data from the form.
+ * @param int    $resource_id The ID of the associated resource.
+ *
+ * @return void
+ */
