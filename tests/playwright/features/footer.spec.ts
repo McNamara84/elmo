354d1944fab91b90fd3f297b6d4b919dbf95e40f
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-12-10 10:47:45 +0100

    added new playrights test for footer

---
+++ tests/playwright/features/footer.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Footer Tests', () => {
+
+  test('footer contains all expected elements', async ({ page }) => {
+    await page.goto('/');
+
+    const footer = page.locator('.footer-info-nav');
+    await expect(footer).toBeVisible();
+
+    const legalNotice = footer.getByText('Legal Notice');
+    const dataProtection = page.locator('#buttonPrivacy');
+    const elmoGuide = page.locator('#buttonHelp');
+
+    // Check visibility
+    await expect(legalNotice).toBeVisible();
+    await expect(dataProtection).toBeVisible();
+    await expect(elmoGuide).toBeVisible();
+    await expect(legalNotice).toHaveAttribute(
+      'href',
+      'https://dataservices.gfz-potsdam.de/web/about-us/legal-notice'
+    );
+    await expect(dataProtection).toHaveAttribute('href', 'doc/privacyPolicy.html');
+    await expect(elmoGuide).toHaveAttribute('href', 'doc/help.html');
+  });
+});
