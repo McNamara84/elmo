ec45f5a8c332ba993db57bec5a652f3f8c83f6b4
Holger Ehrmann <holgerehrmann@gmail.com>
2026-04-17 08:59:49 +0200

    Fix test: double klick > single click

--- tests/playwright/formgroups/elmogem-specific/elmogem-save.spec.ts
+++ tests/playwright/formgroups/elmogem-specific/elmogem-save.spec.ts
@@ -21,7 +21,6 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Filename' }).click();
   await page.getByRole('textbox', { name: 'Filename' }).dblclick();
   await page.getByRole('textbox', { name: 'Filename' }).fill('test_save_with_incoplete_info');
-  await page.getByRole('button', { name: 'Save', exact: true }).click();
   const downloadPromise = page.waitForEvent('download');
   await page.getByRole('button', { name: 'Save', exact: true }).click();
   const download = await downloadPromise;
