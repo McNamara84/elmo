31e2901059ea004ce5ef257a267adc1f55aa66e9
Torhof <torkhov@gfz-potsdam.de>
2026-06-30 10:51:13 +0200

    deleted codegen artifact

--- tests/playwright/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByLabel('Submission not possible').getByText('Close').click();
-});
