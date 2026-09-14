b1747ce95bbffc7a6e19318e074238a55fdf5e43
Torhof <torkhov@gfz-potsdam.de>
2026-09-14 16:39:02 +0200

    remove mock skipping in ICGEM Controller

--- api/v2/controllers/ICGEMController.php
+++ api/v2/controllers/ICGEMController.php
@@ -1,11 +1,6 @@
 <?php
 require_once __DIR__ . '/DatasetController.php';
 
-// Skip class definition if already defined (e.g., mock in tests)
-if (class_exists('ICGEMController', false)) {
-    return;
-}
-
 class ICGEMController extends DatasetController
 {
     private const ICGEM_NAMESPACE_PREFIX = 'grav';
