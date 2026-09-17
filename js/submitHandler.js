88efe3b7b907853fd36eee5ecb2b40b710bf2ad9
Torhof <torkhov@gfz-potsdam.de>
2026-09-17 17:33:47 +0200

    commented out the data upload function call

--- js/submitHandler.js
+++ js/submitHandler.js
@@ -467,7 +467,8 @@ class SubmitHandler {
                     this.showNotification('success',
                         translations.alerts.successHeading,
                         translations.alerts.successMessage);
-
+                    
+                    /* COMMENTED OUT in hotfix/hide-data-upload
                     // Append primary data upload hint if URL is configured
                     const uploadUrl = window.ELMO_FEATURES?.dataUploadUrl;
                     if (uploadUrl) {
@@ -476,7 +477,7 @@ class SubmitHandler {
                         $('#modal-notification-body').append(hint);
                         $('#modal-notification .modal-dialog').addClass('modal-lg');
                     }
-
+                    */
                     if (this.autosaveService) {
                         this.autosaveService.clearDraft();
                     }
